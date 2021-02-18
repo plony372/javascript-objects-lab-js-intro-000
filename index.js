a780b6e1553e2890cb531117df01b2156bb2b931
plony benav <menachemfuterfas@gmail.com>
2021-02-18 16:49:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ function updateObjectWithKeyAndValue(object, key, value){
   var newObj = Object.assign({}, object, {[key]: value});
   return newObj;
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  return object[key] = value
+}
